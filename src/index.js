16da61199104380decd624d1ad566cc46fee46d3
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-10-30 07:45:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const root = document.getElementById('root');
-if (root !== null) {
-  ReactDOM.render(<App />, root);
-}
+
 
 
 
